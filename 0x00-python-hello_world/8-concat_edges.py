0526c264a1d5da46a94de182b6faab898083ca31
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-09-19 22:13:42 +0000

    Adjusted

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = " ".join(str.split()[5:8]) + str[6] + str.split()[12] + str[6]  + str.split()[0]
+str = str[6].join(str.split()[5:8]) + str[6] + str.split()[12] + str[6]  + str.split()[0]
 print(str)
