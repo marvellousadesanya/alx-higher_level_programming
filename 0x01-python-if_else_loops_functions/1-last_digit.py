19ba2e18800cef43deade51f8cef10e8986de310
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-09-20 22:07:58 +0000

    Added prefix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+prefix = f"Last digit of {number}"
 
 # First convert number to string
 repr(number)
@@ -15,9 +16,8 @@ if number < 0:
 
 # Conditional statements
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"{prefix} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"{prefix} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-
+    print(f"{prefix} is {last_digit} and is less than 6 and not 0")
