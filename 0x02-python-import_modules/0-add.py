178f5a6ad0b37469bc445d05688a4704941c9afe
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-09-22 21:51:13 +0000

    Solved

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-import add_0 as add
-add = add.add
+if __name__ == "__main__":
+    import add_0 as add
+    add = add.add
 
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
