93821267aa7597b59e0f045eba69874baab21fdb
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-09-23 04:10:14 +0000

    Bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-from magic_calculation_102 add, sub
+def magic_calculation(a, b):
+    from magic_calculation_102 add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(90):
+            add(c, i)
+            return c
+    else:
+        c = sub(a, b)
+
