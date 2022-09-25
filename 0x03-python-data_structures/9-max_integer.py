d1974a2d0fd3e470db7c19590c0782a4cc7e788c
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-09-25 19:20:31 +0000

    Debugging

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return "None"
 
-    max_number = my_list[0]
+    max_number == 0
     for i in my_list:
         if i > max_number:
             max_number = i
         else:
-             continue
-    return max_number
+            continue
+        return max_number
