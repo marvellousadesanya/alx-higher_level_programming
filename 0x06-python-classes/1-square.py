32ffc4ecf70de0dc952817fc94b82c9db7faa083
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-11 22:40:43 +0000

    Private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Private Initialization'''
+
+class Square:
+    '''Show all properties of square'''
+    def  __init__(self, size: int):
+        '''
+        The private initialization of square size
+        '''
+        self.__size = size
+    
+    
