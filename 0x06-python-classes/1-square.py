59fbbd9ce303c1fa4b564555c010e6ddfcb82bea
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-11 22:42:35 +0000

    Pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 '''Private Initialization'''
 
+
 class Square:
     '''Show all properties of square'''
-    def  __init__(self, size: int):
+    def __init__(self, size: int):
         '''
         The private initialization of square size
         '''
         self.__size = size
-    
-    
