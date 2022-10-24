f0a5cb5db41e23ae3630a6c0197f517d1ebfb247
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-24 23:16:44 +0000

    Done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Sort list
+'''
+
+
+class MyList(list):
+    '''Represents My List'''
+
+    def print_sorted(self):
+       '''Prints list but sorted'''
+       print(sorted(self))
+
