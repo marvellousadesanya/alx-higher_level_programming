3a8e9e1ce9f876349a4c6db11b0f4f3b3833dccc
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-25 20:39:10 +0000

    Super()

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Defines of a subclass Square of subclass Rectangle'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    '''A subclass of Rectangle'''
+    def __init__(self, size):
+        '''Initializing values of size and validation'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
