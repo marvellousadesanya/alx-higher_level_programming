916f7d219216de69bd070c99c2e1956e05c5d720
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-25 19:53:25 +0000

    Pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,6 +4,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     '''Representation of a rectangle'''
     def __init__(self, width, height):
@@ -12,6 +13,7 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
     def area(self):
         return self.__width * self.__height
 
