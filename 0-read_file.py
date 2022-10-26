faa0ba83b1df279bf8b9a3168271f8f8f7a29cc7
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-26 10:32:49 +0000

    Read file

---
+++ 0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A funtion that a file and prints to stdout'''
+
+
+def read_file(filename=""):
+    '''Function to print'''
+    with open(filename, mode="r", encoding="utf-8") as file:
+        print(file.read())
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-higher_level_programming
-Learning about Python
+Input/output Python
