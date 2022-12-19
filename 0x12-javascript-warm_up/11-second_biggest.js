73d74b075ec06d5f4cee7fd0ae0eee3e890dee79
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-12-19 21:57:51 +0000

    Semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
-let args = process.argv
-args.sort(function(a,b) {
-	return a - b;
+const args = process.argv;
+
+args.sort(function (a, b) {
+  return a - b;
 });
 
-if (args.length <= 2) {
+if (args.length <= 3) {
   console.log(0);
 } else {
-    console.log(args[args.length - 2]);
+  console.log(args[args.length - 2]);
 }
