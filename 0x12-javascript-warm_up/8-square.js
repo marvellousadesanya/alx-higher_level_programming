ae53c5591fc388a8f7a54dd85088a727183dd106
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-12-19 20:29:11 +0000

    Semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let size = Number(process.argv[2]);
+const size = Number(process.argv[2]);
 
 if (!Number.isInteger(size) || !size) {
   console.log('Missing size');
 }
 
 for (let j = 0; j < size; j++) {
-    console.log('X'.repeat(size));
+  console.log('X'.repeat(size));
 }
