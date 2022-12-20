60f2f5da464b4b2c51cbf0d1a89b9ff0ceb96bee
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-12-20 17:06:46 +0000

    Print rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,18 +1,15 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
 module.exports = class Rectangle {
   // class constructor
-  constructor(w, h) {
+  constructor (w, h) {
     // check if w and h are positive integers
     if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       // initialize instance attributes
       this.width = w;
       this.height = h;
     } else {
-      // create an empty Rectangle object
-      this.width = 0;
-      this.height = 0;
+      return {};
     }
   }
-}
-
+};
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
+    if (h <= 0 || w <= 0) {
+      return {};
+    }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
