3a1c1949398c597f15cefc7c97ee9472c6e1a8af
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-12-20 20:17:23 +0000

    Done

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,13 +3,10 @@
 module.exports = class Rectangle {
   // class constructor
   constructor (w, h) {
-    // check if w and h are positive integers
     if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       // initialize instance attributes
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
 };
