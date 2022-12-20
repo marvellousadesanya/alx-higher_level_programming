ed04c4cd5e236b3736985cbf1f3be46077abdac1
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-12-20 12:37:38 +0000

    Done

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+    if (w <= 0 || h <= 0) {
+      this.height = this.width = 0;
+    }
+  }
+};
