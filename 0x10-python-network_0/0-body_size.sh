ea95293a2e0989b63e2a968720a8d2a2c50972c9
Marvellous Adesanya <marvellousniyi123@gmail.com>
2023-01-18 03:00:45 +0000

    Done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]
-then
-    echo "Please provide a URL as an argument."
-    exit 1
-fi
-
-# Send a request to the URL using curl and save the response to a variable
-response=$(curl -s "$1")
-
-# Get the size of the response body in bytes
-size=$(echo "$response" | wc -c)
-
-# Display the size of the response body in bytes
-echo "$size"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
 
