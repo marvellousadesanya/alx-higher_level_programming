713e219246941dc57bcaf1f5c2937163e4b6b253
Marvellous Adesanya <marvellousniyi123@gmail.com>
2023-01-17 19:07:26 -0800

    Done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-# Check if a URL is provided as an argument
-if [ -z "$1" ]
-then
-    echo "Please provide a URL as an argument."
-    exit 1
-fi
-
-# Send a GET request to the URL using curl and save the response to a variable
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# Check the status code of the response
-if [ "$response" == "200" ]
-then
-    # Send a GET request to the URL using curl and save the response body to a variable
-    body=$(curl -s "$1")
-    # Display the response body
-    echo "$body"
-else
-    echo "$response"
-fi
-
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
