89032e56806f638a376b41e58dc73fe2337fe25e
Marv Adesanya <marvellousniyi123@gmail.com>
2023-02-09 11:28:23 +0000

    test

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,7 +8,5 @@ const stringToPrint = process.argv[3];
 fs.writeFile(filePath, stringToPrint, 'utf-8', (err) => {
   if (err) {
     return console.error(err);
-  } else {
-    console.log(stringToPrint);
   }
 });
